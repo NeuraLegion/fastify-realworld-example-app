1f8bbfd1c127320795a209800cdead5699b0c10d
Artem Derevnjuk <artedere@gmail.com>
2025-02-08 12:48:50 +0400

    test: generated Bright SecTester e2e security tests

---
+++ test/security/get-articles-feed.test.js
@@ -0,0 +1,44 @@
+'use strict'
+const t = require('tap')
+const { SecRunner, TestType, Severity, HttpMethod, AttackParamLocation } = require('@sectester/runner')
+const startServer = require('../../setup-server')
+
+let runner;
+let server;
+let baseUrl;
+
+t.before(async () => {
+  server = await startServer();
+  baseUrl = await server.listen({ port: 0 });
+});
+
+t.beforeEach(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME
+  });
+  await runner.init();
+});
+
+t.afterEach(() => runner.clear());
+
+t.teardown(() => server.close());
+
+t.test('GET /api/articles/feed', async t => {
+  t.setTimeout(15 * 60 * 1000);
+
+  const promise = runner
+    .createScan({
+      tests: [TestType.JWT, TestType.EXCESSIVE_DATA_EXPOSURE, TestType.ID_ENUMERATION, TestType.HTTP_METHOD_FUZZING],
+      attackParamLocations: [AttackParamLocation.QUERY, AttackParamLocation.HEADER]
+    })
+    .threshold(Severity.LOW)
+    .timeout(15 * 60 * 1000)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/api/articles/feed`,
+      headers: { Authorization: 'Token jwt.token.here' },
+      query: { limit: '20', offset: '0' }
+    });
+
+  await t.resolves(promise);
+});
---
+++ test/security/get-articles.test.js
@@ -0,0 +1,50 @@
+'use strict'
+const t = require('tap')
+const { SecRunner } = require('@sectester/runner')
+const { TestType, Severity, AttackParamLocation, HttpMethod } = require('@sectester/scan')
+const startServer = require('../setup-server')
+
+let runner;
+let server;
+let baseUrl;
+
+t.before(async () => {
+  server = await startServer();
+  baseUrl = await server.listen({ port: 0 });
+});
+
+t.beforeEach(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME
+  });
+  await runner.init();
+});
+
+t.afterEach(() => runner.clear());
+
+t.teardown(() => server.close());
+
+t.test('GET /api/articles', async t => {
+  t.setTimeout(15 * 60 * 1000);
+
+  const promise = runner
+    .createScan({
+      tests: [TestType.SQLI, TestType.XSS, TestType.EXCESSIVE_DATA_EXPOSURE, TestType.ID_ENUMERATION],
+      attackParamLocations: [AttackParamLocation.QUERY]
+    })
+    .threshold(Severity.LOW)
+    .timeout(15 * 60 * 1000)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/api/articles`,
+      query: {
+        tag: 'example',
+        author: 'example',
+        favorited: 'example',
+        limit: '20',
+        offset: '0'
+      }
+    });
+
+  await t.resolves(promise);
+});
