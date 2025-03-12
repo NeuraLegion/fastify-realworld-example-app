d95df2edde0b709e1de59a8b04ba3bde2f01ac37
Artem Derevnjuk <artedere@gmail.com>
2025-03-12 23:59:17 +0400

    test: add auto-generated e2e security tests
    skip-checks:true

---
+++ .brightsec/tests/get-articles.test.ts
@@ -0,0 +1,33 @@
+import { test, before, after } from 'node:test';
+import { Severity, AttackParamLocation, HttpMethod } from '@sectester/scan';
+import { SecRunner } from '@sectester/runner';
+
+let runner!: SecRunner;
+
+before(async () => {
+  runner = new SecRunner({
+    hostname: process.env.BRIGHT_HOSTNAME!,
+    projectId: process.env.BRIGHT_PROJECT_ID!
+  });
+
+  await runner.init();
+});
+
+after(() => runner.clear());
+
+const timeout = 40 * 60 * 1000;
+const baseUrl = process.env.BRIGHT_TARGET_URL!;
+
+test('GET /articles', { signal: AbortSignal.timeout(timeout) }, async () => {
+  await runner
+    .createScan({
+      tests: ['excessive_data_exposure', 'bopla', 'sqli', 'mass_assignment', 'csrf'],
+      attackParamLocations: [AttackParamLocation.QUERY]
+    })
+    .threshold(Severity.CRITICAL)
+    .timeout(timeout)
+    .run({
+      method: HttpMethod.GET,
+      url: `${baseUrl}/articles?tag=technology&author=john_doe&favorited=true&limit=10&offset=0`
+    });
+});
